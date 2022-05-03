13e006227840a0f28060b4c67d19797718f33e6c
Kyaw Zin Thet <96371943+KyawZinThet1996@users.noreply.github.com>
2022-05-03 14:01:16 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,6 @@ async def start(event):
 ┗━━━━━━━━━━━━━━━━━
  
   ʜɪᴛ /help **FOR MORE**
-  /koyee or /ကိုကြီး **About bot uploader**
  [❤](https://telegra.ph/file/2fa3a833f3ccc1d98dba1.jpg),
 """,
     link_preview=False,
@@ -41,17 +40,6 @@ async def start(event):
     ],
     )
   )
-
-@client.on(events.NewMessage(pattern="^/koyee$"))
-async def start(event):
-  await event.reply('Koyee-@life1sfuckcycle is my father uploaded me online')
-
-
-@client.on(events.NewMessage(pattern="^/ကိုကြီး$"))
-async def start(event):
-  await event.reply('Koyee-@life1sfuckcycle is my father uploaded me online')
-
-
 
 @client.on(events.NewMessage(pattern="^/help$"))
 async def help(event):
